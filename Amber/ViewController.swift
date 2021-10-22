50cf2b4ae12fa51868d76d4f2fa377ee655ef056
Alex Studnicka <astudnicka@gmail.com>
2021-10-22 16:48:30 +0200

    Initial Commit

---
+++ Amber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Amber
+//
+//  Created by Alex Studnicka on 22.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
